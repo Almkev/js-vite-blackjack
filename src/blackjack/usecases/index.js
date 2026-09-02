5ca352b4a786b66ebe9079121ca50c0d21d3a375
Kevin Almonte <almkevin02@hotmail.com>
2026-09-01 20:26:04 -0400

    primer commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck as crearNuevoDeck } from "./crear-deck.js";
+export { pedirCarta } from "./pedir-carta.js";
+export { valorCarta } from "./valor-carta.js";
+export { turnoComputadora } from "./turno-computadura.js";
+export { crearCartaHTML } from "./crear-carta.js";
---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+import _ from "underscore";
+
+/**
+ *
+ * @param {Array<String>} deck es un arreglo de string
+ * @returns {String}
+ */
+export const pedirCarta = (deck) => {
+  if (deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
+  const carta = deck.pop();
+  return carta;
+};
